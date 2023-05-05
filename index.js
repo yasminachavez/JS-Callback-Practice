793cd5adbd0b74b0d65de1ac7cc760bf790c0bc2
yasminachavez <yasminachavez@yahoo.com>
2023-05-04 18:09:04 -0700

    finished activity maybe

--- index.js
+++ index.js
@@ -5,26 +5,47 @@ const character = newImage('assets/green-character/static.gif')
 let direction = null;
 let x = 100;
 let y = 250;
-move(character).withArrowKeys(100, 250)
+move(character).to(100,250)
 
-setInterval(function(){
-if (direction === 'west'){
+function moveCharacter (){
+if(direction === 'west'){
     x = x - 1
 }
-if (direction === 'east'){
-    x = x + 1
-}
-if (direction === 'north'){
+if(direction === 'north'){
     y = y + 1
 }
-if (direction === 'south'){
+if(direction === 'east'){
+    x = x + 1
+}
+if(direction === 'south'){
     y = y - 1
 }
 character.style.left = x + 'px'
-character.style.right = y + 'px'
+character.style.bottom = y + 'px'
+}
+
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
 }, 1)
 
-document.addEventListener('keydown', function(e){  
+document.addEventListener('keydown', function(e){
+
+})
+
+document.addEventListener('keydown', function(e){
     if(e.repeat) return;
 
     if(e.key === 'ArrowLeft'){
@@ -40,105 +61,10 @@ document.addEventListener('keydown', function(e){
         direction = 'south'
     }
 })
+
 document.addEventListener('keyup', function(e){
     direction = null
 })
-
-function moveWithArrowKeys(left, bottom, callback){
-    let direction = null;
-    let x = left;
-    let y = bottom;
- 
-    document.addEventListener('keydown', function(e){
-        if(e.repeat) return;
-    
-        if(e.key === 'ArrowLeft'){
-            direction = 'west'
-        }
-        if(e.key === 'ArrowUp'){
-            direction = 'north'
-        }
-        if(e.key === 'ArrowRight'){
-            direction = 'east'
-        }
-        if(e.key === 'ArrowDown'){
-            direction = 'south'
-        }
-        handleDirectionChange()
-    })
-    
-    document.addEventListener('keyup', function(e){
-        direction = null
-        handleDirectionChange()
-    })
-    
-
-    element.style.left = x + 'px'
-    element.style.bottom = y + 'px'
-    
-    function moveCharacter(){ 
-        if(direction === 'west'){
-            x-=1
-        }
-        if(direction === 'north'){
-            y+=1
-        }
-        if(direction === 'east'){
-            x+=1
-        }
-        if(direction === 'south'){
-            y-=1
-        }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
-    }
-    
-    setInterval(moveCharacter, 1)
-    
-    document.addEventListener('keydown', function(e){
-        if(e.repeat) return;
-    
-        if(e.key === 'ArrowLeft'){
-            direction = 'west'
-        }
-        if(e.key === 'ArrowUp'){
-            direction = 'north'
-        }
-        if(e.key === 'ArrowRight'){
-            direction = 'east'
-        }
-        if(e.key === 'ArrowDown'){
-            direction = 'south'
-        }
-    })
-    
-    document.addEventListener('keyup', function(e){
-        direction = null
-    })
-}
-
-const character = newImage('assets/green-character/static.gif')
-
-function handleDirectionChange(){
-    if(direction === null){
-        character.src = 'assets/green-character/static.gif'
-    }
-    if(direction === 'west'){
-        character.src = 'assets/green-character/west.gif'
-    }
-    if(direction === 'north'){
-        character.src = 'assets/green-character/north.gif'
-    }
-    if(direction === 'east'){
-        character.src = 'assets/green-character/east.gif'
-    }
-    if(direction === 'south'){
-        character.src = 'assets/green-character/south.gif'
-    }
-}
-
-move(character).withArrowKeys(100, 250, handleDirectionChange)
-
 
 
 move(newImage('assets/tree.png')).to(200, 450)
@@ -150,24 +76,5 @@ move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
 
-const character = newImage('assets/green-character/static.gif')
 
-function handleDirectionChange(direction){
-    if(direction === null){
-        character.src = 'assets/green-character/static.gif'
-    }
-    if(direction === 'west'){
-        character.src = 'assets/green-character/west.gif'
-    }
-    if(direction === 'north'){
-        character.src = 'assets/green-character/north.gif'
-    }
-    if(direction === 'east'){
-        character.src = 'assets/green-character/east.gif'
-    }
-    if(direction === 'south'){
-        character.src = 'assets/green-character/south.gif'
-    }
-}
 
-move(character).withArrowKeys(100, 250, handleDirectionChange)
